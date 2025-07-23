9fc7dd1d720b06909f10af60c8c0e2abf51e7da0
jdpp95 <jdporrasp@gmail.com>
2025-07-22 21:22:21 -0500

    Revert "Use session storage instead of local storage"
    This reverts commit 5bcf090c485e0c93e1600bbe8ddd10d9b7dd28ca.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -137,7 +137,7 @@ export class AppComponent implements OnInit {
     initDate.startOf('day');
     this.onDateChange(initDate.format())
 
-    const savedFormData = sessionStorage.getItem('locationFormData');
+    const savedFormData = localStorage.getItem('locationFormData');
     if (savedFormData) {
       const formData = JSON.parse(savedFormData);
       this.locationForm = new FormGroup({
@@ -194,7 +194,7 @@ export class AppComponent implements OnInit {
             coordsControl.setValue(coords);
             this.coords = coords;
 
-            this.saveFormDataToSessionStorage();
+            this.saveFormDataToLocalStorage();
             this.getWeather();
           }
         )
@@ -219,7 +219,7 @@ export class AppComponent implements OnInit {
         );
       } else {
         this.coords = this.locationForm.value.coords;
-        this.saveFormDataToSessionStorage();
+        this.saveFormDataToLocalStorage();
         this.getWeather();
       }
     }
@@ -229,7 +229,7 @@ export class AppComponent implements OnInit {
     }
   }
 
-  saveFormDataToSessionStorage() {
+  saveFormDataToLocalStorage() {
     const formData = {
       coords: this.locationForm.value.coords,
       now: this.locationForm.value.now,
@@ -238,7 +238,7 @@ export class AppComponent implements OnInit {
       minute: this.locationForm.value.minute,
       UTC: this.locationForm.value.UTC
     };
-    sessionStorage.setItem('locationFormData', JSON.stringify(formData));
+    localStorage.setItem('locationFormData', JSON.stringify(formData));
   }
 
   getWeather() {
